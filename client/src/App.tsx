e25b7951a616fdf55ca988357f7e64ec4adc142c
Ivanosky <joyivan6@gmail.com>
2023-07-15 23:22:03 +0700

    Add register and loginpage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Register from './pages/Register'
+import Login from './pages/Login'
 
 const App:React.FC = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/> 
+        <Route path='/register' element={<Register />}/>
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Register: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-50">
+      <div className="bg-white shadow-lg rounded-lg px-8 py-6 max-w-2xl w-full">
+        <h2 className="text-2xl font-bold mb-6">Sign Up</h2>
+        <form>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              id="username"
+              type="text"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500"
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="flex justify-between">
+            <Link to={'/login'} className='font-bold text-gray-500 cursor-pointer'>Already have an account</Link>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Register
